6b1dafb74ab444b377006d798ec087f73d254718
Aqil <aqilll083@gmail.com>
2023-06-11 07:28:43 +0400

    feat: create `style.css`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '$lib/style.css';
+</script>
+
+<h1>TODO</h1>
+<p>Add Todos and manage them!</p>
