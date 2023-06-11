3c2b77711193d221199dbcf612893e1fefaeef4f
Aqil <aqilll083@gmail.com>
2023-06-11 08:24:56 +0400

    feat: complete header

---
+++ src/lib/components/GithubLink.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import github_mark from '$lib/assets/github-mark-white.svg';
+</script>
+
+<a href="https://github.com/test2user-aqil/todo" target="_blank" rel="noopener noreferrer">
+	<img src={github_mark} alt="" />
+	test2user-aqil/todo
+</a>
+
+<style>
+	a {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		padding: 0.5rem 1rem;
+		gap: 0.5rem;
+		background: var(--dark-1);
+		filter: drop-shadow(var(--drop-shadow));
+		border-radius: 1rem;
+		color: var(--fg);
+		text-decoration: none;
+		font-family: 'Roboto Mono', monospace;
+		font-size: 0.875rem;
+	}
+
+	a img {
+		height: 1rem;
+		width: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,39 @@
 <script lang="ts">
 	import '$lib/style.css';
+	import GithubLink from '$lib/components/GithubLink.svelte';
 </script>
 
-<h1>TODO</h1>
-<p>Add Todos and manage them!</p>
+<svelte:head>
+	<title>Todo &bull; test2user-aqil</title>
+</svelte:head>
+
+<div class="container">
+	<div class="header">
+		<h1>TODO</h1>
+		<p>Add Todos and manage them!</p>
+		<GithubLink />
+	</div>
+
+	<div class="content">
+		<div class="input" />
+		<div class="todos" />
+	</div>
+</div>
+
+<style>
+	.container {
+		max-width: 700px;
+		margin: 0 auto;
+		padding: 4rem 0.625rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+	}
+</style>
